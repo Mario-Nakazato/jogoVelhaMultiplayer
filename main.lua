cbf5b607358238f7eb8c5c1260a6c97cd6b6b0f3
Mario Nakazato <marionakazat@gmail.com>
2021-10-31 19:49:35 -0300

    Ajustes main.lua e conf.lua

--- main.lua
+++ main.lua
@@ -1,172 +1,172 @@
 function love.load(arg)
-  
-  if arg[#arg] == "-debug" then require("mobdebug").start() end -- Debug para ZeroBrane Studio IDE Utilize; Argumento - arg esta disponivel global.
-  
-  
-  
+
+    if arg[#arg] == "-debug" then require("mobdebug").start() end -- Debug para ZeroBrane Studio IDE Utilize; Argumento - arg esta disponivel global.
+
+
+
 end
 
 function love.update(dt)
-  
-  
-  
+
+
+
 end
 
 function love.draw()
-  
-  
-  
+
+
+
 end
 
 function love.keypressed(tecla, cod, repeticao)
-  
-  
-  
+
+
+
 end
 
 function love.keyreleased(tecla, cod)
-  
-  
-  
+
+
+
 end
 
 function love.mousepressed(x, y, botao, toque, repeticao)
-  
-  
-  
+
+
+
 end
 
 function love.mousereleased(x, y, botao, toque, repeticao)
-  
-  
-  
+
+
+
 end
 
 function love.mousemoved(x, y, dx, dy, toque)
-  
-  
-  
+
+
+
 end
 
 function love.wheelmoved(x, y)
-  
-  
-  
+
+
+
 end
 
 function love.mousefocus(foco)
-  
-  
-  
+
+
+
 end
 
 function love.resize(c, l)
-  
-  
-  
+
+
+
 end
 
 function love.focus(foco)
-  
-  
-  
+
+
+
 end
 
 function love.quit()
-  
-  
-  
+
+
+
 end
 --[[
 function inicioContato(a, b, contato)
-  
-  
-  
+
+
+
 end
 
 function fimContato(a, b, contato)
-  
-  
-  
+
+
+
 end
 
 function preContato(a, b, contato)
-  
-  
-  
+
+
+
 end
 
 --function posContato(a, b, contato, normalImpulso, tangenteImpulso, normalImpulso1, tangenteImpulso1)
 function posContato(a, b, contato, normalImpulso, tangenteImpulso)
-  
-  
-  
+
+
+
 end
 
 function love.touchpressed(id, x, y, dx, dy, pressao)
-  
-  
-  
+
+
+
 end
 
 function love.touchreleased(id, x, y, dx, dy, pressao)
-  
-  
-  
+
+
+
 end
 
 function love.touchmoved(id, x, y, dx, dy, pressao)
-  
-  
-  
+
+
+
 end
 
 function love.displayrotated(indice, orientacao)
-  
-  
-  
+
+
+
 end
 
 function love.textedited(texto, inicio, tamanho)
-  
-  
-  
+
+
+
 end
 
 function love.textinput(texto)
-  
-  
-  
+
+
+
 end
 
 function love.directorydropped(caminho)
-  
-  
-  
+
+
+
 end
 
 function love.filedropped(arquivo)
-  
-  
-  
+
+
+
 end
 
 function love.errorhandler(erro)
-  
-  
-  
+
+
+
 end
 
 function love.lowmemory()
-  
-  
-  
+
+
+
 end
 
 function love.threaderror(thread, erro)
-  
-  
-  
+
+
+
 end
 
 function love.visible(visivel)-- Esta funcao CallBack não funciona, utilize visivel = love.window.isMinimized()
