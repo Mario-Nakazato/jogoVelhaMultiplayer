71625dfb4eac1ceafd556606d9e4f211a010ca8b
Bruno Brandão Silva <72681281+Bruno-Brandao-Silva@users.noreply.github.com>
2021-11-01 00:18:12 -0300

    Again 2

--- main.lua
+++ main.lua
@@ -4,13 +4,13 @@ require "menu"
 function love.load(arg)
 
     if arg[#arg] == "-debug" then require("mobdebug").start() end -- Debug para ZeroBrane Studio IDE Utilize; Argumento - arg esta disponivel global.
-
-    jogo = tabuleiro.novo()
+    tamanho = 128
+    jogo = tabuleiro.novo(tela.c /2 -tamanho *3 /2, tela.l /2 -tamanho *3 /2, tamanho)
     jogo:load()
     _menu = menu.new()
     _menu:load()
     opc = nil
-    --print
+    
 end
 
 function love.update(dt)
