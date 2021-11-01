553a32ea6824988c379380033539f7567c046677
Mario Nakazato <marionakazat@gmail.com>
2021-10-31 22:12:46 -0300

    Posição escala clique

--- main.lua
+++ main.lua
@@ -5,8 +5,8 @@ function love.load(arg)
 
     if arg[#arg] == "-debug" then require("mobdebug").start() end -- Debug para ZeroBrane Studio IDE Utilize; Argumento - arg esta disponivel global.
 
-    jogo = tabuleiro.novo()
-    jogo:load()
+    tamanho = 128
+    jogo = tabuleiro.novo(tela.c /2 -tamanho *3 /2, tela.l /2 -tamanho *3 /2, tamanho)
 
 end
 
@@ -36,7 +36,7 @@ end
 
 function love.mousepressed(x, y, botao, toque, repeticao)
 
-
+    jogo:mousepressed(x, y, botao, toque, repeticao)
 
 end
 
--- tabuleiro.lua
+++ tabuleiro.lua
@@ -8,36 +8,84 @@
 
 require "palavra"
 
-local function novo()
-  
+local function novo(x, y, tamanho)
+
     local tabuleiro = {
-        fonte = lgrafico.newFont(128)
+        x = x or 0,
+        y = y or 0,
+        tamanho = tamanho or 128,
+        quadrado = {
+            [1] = {" ", " ", " "},
+            [2] = {" ", " ", " "},
+            [3] = {" ", " ", " "},
+        }
     }
+
+    tabuleiro.fonte = lgrafico.newFont(tabuleiro.tamanho)
     
     function tabuleiro:load()
-        lgrafico.setFont(self.fonte)
     end
 
     function tabuleiro:update(dt)
     end
 
     function tabuleiro:draw()
-        lgrafico.setLineWidth(4)
-        lgrafico.line(0, 128, 128 *3, 128)
-        lgrafico.line(0, 128 *2, 128 *3, 128 *2)
-        lgrafico.line(128, 0, 128, 128 *3)
-        lgrafico.line(128 *2, 0, 128 *2, 128 *3)
-        lgrafico.print("O", 16, -8)
+        lgrafico.setLineWidth(self.tamanho /32)
+        lgrafico.line(self.x, self.y +self.tamanho, self.x +self.tamanho *3, self.y +self.tamanho)
+        lgrafico.line(self.x, self.y +self.tamanho *2, self.x +self.tamanho *3, self.y +self.tamanho *2)
+        lgrafico.line(self.x +self.tamanho, self.y, self.x +self.tamanho, self.y +self.tamanho *3)
+        lgrafico.line(self.x +self.tamanho *2, self.y, self.x +self.tamanho *2, self.y +self.tamanho *3)
+        for i = 0, 2 do
+            for j = 0, 2 do
+                lgrafico.print(self.quadrado[i +1][j +1], self.fonte, self.x +self.tamanho *j +self.tamanho /8, self.y +self.tamanho *i -self.tamanho /16)
+            end
+        end
+    end
+
+    function tabuleiro:peca(i, j, p)
+        self.quadrado[i][j] = p or " "
+    end
+
+    function tabuleiro:mousepressed(x, y, botao, toque, repeticao)
+        
+        p = botao == 1 and "X" or "O"
+
+        if self.x < x and self.x +self.tamanho > x then
+            if self.y < y and self.y +self.tamanho > y then
+                self:peca(1, 1, p)
+            elseif self.y +self.tamanho < y and self.y +self.tamanho *2 > y then
+                self:peca(2, 1, p)
+            elseif self.y +self.tamanho *2 < y and self.y +self.tamanho *3 > y then
+                self:peca(3, 1, p)
+            end
+        elseif self.x +self.tamanho < x and self.x +self.tamanho *2 > x then
+            if self.y < y and self.y +self.tamanho > y then
+                self:peca(1, 2, p)
+            elseif self.y +self.tamanho < y and self.y +self.tamanho *2 > y then
+                self:peca(2, 2, p)
+            elseif self.y +self.tamanho *2 < y and self.y +self.tamanho *3 > y then
+                self:peca(3, 2, p)
+            end
+        elseif self.x +self.tamanho *2 < x and self.x +self.tamanho *3 > x then
+            if self.y < y and self.y +self.tamanho > y then
+                self:peca(1, 3, p)
+            elseif self.y +self.tamanho < y and self.y +self.tamanho *2 > y then
+                self:peca(2, 3, p)
+            elseif self.y +self.tamanho *2 < y and self.y +self.tamanho *3 > y then
+                self:peca(3, 3, p)
+            end
+        end
+
     end
 
     return tabuleiro
-  
-  end
-  
-  tabuleiro = {
+
+end
+
+tabuleiro = {
     novo = novo
-  }
+}
+
+print("tabuleiro.lua")
   
-  print("tabuleiro.lua")
-  
-  return tabuleiro
+return tabuleiro
