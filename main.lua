4086be9f0462e13b744c7e1aa65a3864c9848e68
Bruno Brandão Silva <72681281+Bruno-Brandao-Silva@users.noreply.github.com>
2021-11-03 22:14:22 -0300

    teste de conexao

--- main.lua
+++ main.lua
@@ -29,7 +29,41 @@ function love.draw()
     elseif opc == 2 then
         _multi:draw()
         if mopc == 1 then
+            local enet = require "enet"
+            local host = enet.host_create()
+            local server = host:connect("localhost:6789")
+            while true do
+            local event = host:service(100)
+            while event do
+                if event.type == "receive" then
+                print("Got message: ", event.data, event.peer)
+                event.peer:send( "ping" )
+                elseif event.type == "connect" then
+                print(event.peer, "connected.")
+                event.peer:send( "ping" )
+                elseif event.type == "disconnect" then
+                print(event.peer, "disconnected.")
+                end
+                event = host:service()
+            end
+            end
         elseif mopc == 2 then
+            local enet = require "enet"
+            local host = enet.host_create("localhost:6789")
+            while true do
+            local event = host:service(100)
+            while event do
+                if event.type == "receive" then
+                print("Got message: ", event.data, event.peer)
+                event.peer:send( "pong" )
+                elseif event.type == "connect" then
+                print(event.peer, "connected.")
+                elseif event.type == "disconnect" then
+                print(event.peer, "disconnected.")
+                end
+                event = host:service()
+            end
+            end
         elseif mopc == 3 then
             opc = nil
             mopc = nil
